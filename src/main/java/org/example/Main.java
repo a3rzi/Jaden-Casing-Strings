f5e5abef1ed552cdd20b49db5a25ba5971373434
a3rzi <amirhosseinqatar@gmail.com>
2024-01-21 19:47:14 +0330

    step1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //given
+        String phrase = "How can mirrors be real if our eyes aren't real";
+
+        //when
+        String result = toJadenCase(phrase);
+    }
+
+    private static String toJadenCase(String phrase) {
+        //empty string
+        if (phrase == null || phrase.isEmpty()){
+            return null;
+        }
+        //split
+
+        //capitalize
+        //sentence
+
+        return null;
     }
 }
